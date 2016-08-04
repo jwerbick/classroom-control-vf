4d77cb25f40040563de94911ade9cedc61d28525
Jarod Werbick <bombrat@gmail.com>
2016-08-04 09:56:29 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -45,6 +45,10 @@ node default {
   include memcached
   include nginx
   
+  if $::virtual != 'physical' {
+    $vmname = capitalize($::virtual)
+    notify { "This is a ${vmname} virtual machine.": }
+  }
   notify { "Hello, my name is ${::hostname}": }
   notify { "Are we having fun yet?": }
 }
